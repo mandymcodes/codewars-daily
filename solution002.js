c880891ffaa84ef810b2bec44c9c17c3dafeb8c6
Mandy <luckybbhere@gmail.com>
2022-05-12 13:10:13 -0700

    solutions002.js completed - first try

---
+++ solution002.js
@@ -0,0 +1,14 @@
+/* /* Description:
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34 
+
+
+Answer: */
+function opposite(number) {
+return -number
+}
